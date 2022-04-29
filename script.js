dcfc957f7b5ffa78d894a7223b75fc329a48e0e7
Olie Auger <olie.auger@gmail.com>
2022-04-29 09:01:13 -0400

    forgot this thing had git

--- script.js
+++ script.js
@@ -3,8 +3,14 @@
 let perlin = new perlinNoise3d();
 perlin.noiseSeed(Math.random());
 
-const canvas = document.querySelector('canvas');
+// let o = Date.now; Date.now = () => o() / 5;
+
+const canvas = document.querySelector('#main');
+const colorCanvas = document.querySelector('#color');
+/** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext('2d');
+/** @type {CanvasRenderingContext2D} */
+const colorCtx = colorCanvas.getContext('2d');
 
 let areLasersNear = false;
 let playerDead = false;
@@ -42,10 +48,10 @@ class Laser {
 }
 
 class Fragment {
-    constructor(x, y, color) {
+    constructor(x, y, color, angle) {
         this.x = x;
         this.y = y;
-        this.angle = Math.random() * Math.PI * 2;
+        this.angle = angle || (Math.random() * Math.PI * 2);
         this.dx = Math.cos(this.angle) * (Math.random() * 1 + .5) * 300;
         this.dy = Math.sin(this.angle) * (Math.random() * 1 + .5) * 300;
         this.lifetime = 5;
@@ -130,8 +136,23 @@ class Ship {
         ctx.lineTo(this.x + Math.cos(angle + Math.PI + Math.PI / 5) * 20 * (this.dead ? (this.timeSinceDeath + 1) : 1) + dx, this.y + Math.sin(angle + Math.PI + Math.PI / 5) * 20 * (this.dead ? (this.timeSinceDeath + 1) : 1) + dy);
         ctx.lineTo(this.x + Math.cos(angle + Math.PI + Math.PI / 5) * 20 * (this.dead ? (this.timeSinceDeath + 1) : 1), this.y + Math.sin(angle + Math.PI + Math.PI / 5) * 20 * (this.dead ? (this.timeSinceDeath + 1) : 1));
         ctx.closePath();
+        if (dashThisFrame) {
+            let amountOfMovement = 1000 * 0.35;
+            let dx = Math.cos(dashAngle);
+            let dy = Math.sin(dashAngle);
+            let dashX = x;
+            let dashY = y;
+            for (let i = 0; i < amountOfMovement; i += 1.5) {
+                if (ctx.isPointInPath(dashX + 250 - x, dashY + 250 - y) || ctx.isPointInPath(dashX + 5 + 250 - x, dashY + 250 - y) || ctx.isPointInPath(dashX + 250 - x, dashY + 5 + 250 - y) || ctx.isPointInPath(dashX - 5 + 250 - x, dashY + 250 - y) || ctx.isPointInPath(dashX + 250 - x, dashY - 5 + 250 - y)) {
+                    this.shouldDie = true;
+                }
+                dashX += dx * 1.5;
+                dashY += dy * 1.5;
+            }
+        }
         for (let i = 0; i < lasers.length; i++) {
-            if (!this.dead && lasers[i].type == 'player' && ctx.isPointInPath(lasers[i].x + 250 - x, lasers[i].y + 250 - y)) {
+            if (!this.dead && lasers[i].type == 'player' && ctx.isPointInPath(lasers[i].x + 250 - x, lasers[i].y + 250 - y) || this.shouldDie) {
+                this.shouldDie = false;
                 this.dead = true;
                 kills++;
                 score += 10;
@@ -171,6 +192,7 @@ let dx = 0;
 let dy = 0;
 let shoot = false;
 let cooldown = 0.5;
+let dashCooldown = 0;
 
 let mouseX = 0, mouseY = 0;
 canvas.addEventListener('click', e => {
@@ -198,16 +220,32 @@ canvas.addEventListener('mousemove', e => {
 let keys = {
     space: true
 };
+let dashAngle, displayCooldownTime;
 
 addEventListener('keydown', e => {
-    if (e.key == ' ') keys.space = false;
+    // if (e.key == ' ') keys.space = false;
+    if (e.key == ' ') {
+        if (dashCooldown <= 0) {
+            dashCooldown = 4;
+            enteredDash = true;
+            timeInDash = 0;
+            dashAngle = Math.atan2(mouseY, mouseX);
+            dashThisFrame = true;
+            displayCooldown = true;
+            displayCooldownTime = 0.85;
+        } else {
+            displayCooldown = true;
+            displayCooldownTime = 0.85;
+        }
+    }
 });
 
 addEventListener('keyup', e => {
-    if (e.key == ' ') keys.space = true;
+    // if (e.key == ' ') keys.space = true;
 });
 
 let ships = [], lasers = [], fragments = [];
+let enteredDash = false, timeInDash = 0, dashThisFrame = false;
 
 let time = Date.now();
 
@@ -218,6 +256,10 @@ let requiredKills = 4;
 let distance2d = (x1, y1, x2, y2) => Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 
 let timeSincePlayerDeath = 0;
+let isEditorOpen = false;
+let displayCooldown = false;
+
+let spawnRate = 5;
 
 function step() {
     // if (playerDead) {
@@ -254,6 +296,7 @@ function step() {
     let delay = now - time;
     time = now;
     delta = delay / 1000;
+    let doUpdate = !isEditorOpen && !enteredDash;
 
     if (playerDead) {
         delta /= 1 + (timeSincePlayerDeath * 5);
@@ -267,30 +310,40 @@ function step() {
         timeSincePlayerDeath += delta;
     }
 
-    q += delta;
-    if (q > 5) {
-        let isBig = false;
-        if (kills >= requiredKills && timeUntilBigger < 0) {
-            timeUntilBigger = 23;
-            isBig = true;
-        }
-        ships.push(new Ship(Math.random() * 800 - 400 + x, Math.random() * 800 - 400 + y, isBig));
-        q = 0;
+    timeInDash += delta;
+    if (timeInDash > 0.35) {
+        enteredDash = false;
     }
-    cooldown -= delta;
-    if (shoot && cooldown <= 0) {
-        cooldown = 0.5;
-        lasers.push(new Laser(x, y, Math.atan2(mouseY, mouseX), '#00ffdd', 'player'));
-    }
-    shoot = false;
 
-    if (kills > 5) {
-        timeUntilBigger -= delta;
+    if (doUpdate) {
+        dashCooldown -= delta;
+        displayCooldownTime -= delta;
+        q += delta;
+        spawnRate -= delta / (100 / spawnRate);
+        if (q > spawnRate) {
+            let isBig = false;
+            if (kills >= requiredKills && timeUntilBigger < 0) {
+                timeUntilBigger = 23;
+                isBig = true;
+            }
+            ships.push(new Ship(Math.random() * 800 - 400 + x, Math.random() * 800 - 400 + y, isBig));
+            q = 0;
+        }
+        cooldown -= delta;
+        if (shoot && cooldown <= 0) {
+            cooldown = spawnRate / 10;
+            lasers.push(new Laser(x, y, Math.atan2(mouseY, mouseX), '#00ffdd', 'player'));
+        }
+        shoot = false;
+
+        if (kills > 5) {
+            timeUntilBigger -= delta;
+        }
     }
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // ctx.fillStyle = 'black';
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.save();
     ctx.translate(-x + 250, -y + 250);
@@ -313,7 +366,7 @@ function step() {
     }
     // ctx.fillRect(mouseX + 240, mouseY + 240, 10, 10);
     for (let i = 0; i < ships.length; i++) {
-        if (!playerDead) {
+        if (!playerDead && doUpdate) {
             ships[i].update(delta);
         }
         ships[i].draw(ctx);
@@ -324,7 +377,9 @@ function step() {
     }
 
     outer: for (let i = 0; i < lasers.length; i++) {
-        lasers[i].update(delta);
+        if (doUpdate) {
+            lasers[i].update(delta);
+        }
         lasers[i].draw(ctx);
         if (distance2d(lasers[i].x, lasers[i].y, x, y) > 1000) {
             for (let j = 0; j < ships.length; j++) {
@@ -338,7 +393,9 @@ function step() {
     }
 
     for (let i = 0; i < fragments.length; i++) {
-        fragments[i].update(delta);
+        if (doUpdate) {
+            fragments[i].update(delta);
+        }
         fragments[i].draw(ctx);
         if (distance2d(fragments[i].x, fragments[i].y, x, y) > 1000 || fragments[i].lifetime < 0) {
             fragments.splice(i, 1);
@@ -396,13 +453,37 @@ function step() {
     ctx.arc(250, 250, 100, angle - Math.PI / 40, angle + Math.PI / 40);
     ctx.stroke();
 
+    for (let i = 0; i < ships.length; i++) {
+        ctx.strokeStyle = ships[i].isBig ? '#0000ff88' : '#00009988';
+        ctx.lineWidth = 5 + 10 ** timeSincePlayerDeath;
+        let angle = Math.atan2(ships[i].y - y, ships[i].x - x);
+        ctx.beginPath();
+        ctx.arc(250, 250, 100, angle - Math.PI / 40, angle + Math.PI / 40);
+        ctx.stroke();
+    }
+
     ctx.fillStyle = 'white';
     ctx.fillRect((250 + mouseX - (10)) | 0, (250 + mouseY) | 0, 20, 1);
     ctx.fillRect((250 + mouseX) | 0, (250 + mouseY - (10)) | 0, 1, 20);
     ctx.font = '15px "DIN 1451"';
     ctx.textAlign = 'right';
-    ctx.fillText(`SCORE ${score}`, 495, 20);
-    if (!playerDead) {
+    ctx.fillText(`SCORE ${score} SR ${spawnRate.toFixed(2)}`, 495, 20);
+    if (displayCooldown) {
+        if (dashCooldown < 0) {
+            displayCooldown = false;
+        } else {
+            ctx.save();
+            ctx.translate(250, 300);
+            ctx.rotate(Math.random() * Math.PI / 25 - Math.PI / 50);
+            ctx.strokeStyle = 'red';
+            ctx.lineWidth = 4;
+            ctx.strokeRect(-50, -10, 100, 20);
+            ctx.fillStyle = 'white';
+            ctx.fillRect(-48, -8, 96 * (1 - (dashCooldown / 4)), 16);
+            ctx.restore();
+        }
+    }
+    if (!playerDead && doUpdate) {
         let movementX = Math.cos(angle) * (keys.space ? 2 : 1);
         let movementY = Math.sin(angle) * (keys.space ? 2 : 1);
         dx += movementX * (keys.space ? delta * 2 : delta);
@@ -416,6 +497,76 @@ function step() {
         x += dx * delta * 170;
         y += dy * delta * 170;
     }
+
+    if (enteredDash) {
+        let amount = timeInDash > 0.175 ? (0.35 - timeInDash) : timeInDash;
+        let data = ctx.getImageData(0, 0, 500, 500);
+        let data2 = colorCtx.createImageData(500, 500);
+        let distance = -Math.sin(amount * (1 / 0.175) * Math.PI * 2) * 12;
+        let translateX = Math.cos(dashAngle + Math.PI / 2) * distance;
+        let translateY = Math.sin(dashAngle + Math.PI / 2) * distance;
+        for (let i = 0; i < data.data.length; i += 4) {
+            data2.data[i] = 0;
+            data2.data[i + 1] = data.data[i + 1];
+            data2.data[i + 2] = 0;
+            data2.data[i + 3] = data.data[i + 3] * 0.8;
+        }
+        ctx.putImageData(data2, 0, 0);
+        for (let i = 0; i < data.data.length; i += 4) {
+            data2.data[i] = data.data[i];
+            data2.data[i + 1] = 0;
+            data2.data[i + 2] = 0;
+            data2.data[i + 3] = data.data[i + 3];
+        }
+        colorCtx.putImageData(data2, 0, 0);
+        ctx.globalAlpha = 0.8;
+        ctx.drawImage(colorCanvas, -translateX, -translateY);
+        for (let i = 0; i < data.data.length; i += 4) {
+            data2.data[i] = 0;
+            data2.data[i + 1] = 0;
+            data2.data[i + 2] = data.data[i + 2];
+        }
+        colorCtx.putImageData(data2, 0, 0);
+        ctx.drawImage(colorCanvas, translateX, translateY);
+        ctx.globalAlpha = 1;
+        let dx = Math.cos(dashAngle);
+        let dy = Math.sin(dashAngle);
+        x += dx * delta * 1000;
+        y += dy * delta * 1000;
+    }
+    // let imageData = ctx.getImageData(0, 0, 500, 500);
+    // let data = imageData.data;
+    // let otherImageData = ctx.getImageData(0, 0, 500, 500);
+    // let otherData = otherImageData.data;
+    /*for (let x = 10; x < 490; x += 1) {
+        // break;
+        for (let y = 10; y < 490; y += 3) {
+            let index = (x + y * 500) * 4;
+            let r = data[index];
+            let g = data[index + 1];
+            let b = data[index + 2];
+            let a = data[index + 3];
+            if (a != 255) {
+                for (let u = -4; u < 5; u++) {
+                    for (let v = -9; v < 10; v++) {
+                        let index_ = ((x + u) + (y + v) * 500) * 4;
+                        if (otherData[index_ + 3] > 0) {
+                            r += otherData[index_] / (Math.sqrt(u * u + v * v) + 40);
+                            g += otherData[index_ + 1] / (Math.sqrt(u * u + v * v) + 40);
+                            b += otherData[index_ + 2] / (Math.sqrt(u * u + v * v) + 40);
+                            a += otherData[index_ + 3] / (Math.sqrt(u * u + v * v) + 40);
+                        }
+                    }
+                }
+            }
+            data[index] = r;
+            data[index + 1] = g;
+            data[index + 2] = b;
+            data[index + 3] = a;
+        }
+    }*/
+    // ctx.putImageData(imageData, 0, 0);
+    dashThisFrame = false;
 }
 
 step();
